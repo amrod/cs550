ac6714d79933a07c571e1d7544ff77f3bfb45254
Amaury Rodriguez <amaury.rodriguez@gmail.com>
2015-10-04 00:53:34 -0400

    add copy and raw constructors

--- hw2/src/edu/sju/MyPolynomial.java
+++ hw2/src/edu/sju/MyPolynomial.java
@@ -1,10 +1,36 @@
 package edu.sju;
 
-public class MyPolynomial {
-    LinkedList <Integer[]> poly;
+import java.util.Iterator;
 
-    public MyPolynomial(){
-        poly = new LinkedList<>();
+public class MyPolynomial implements Iterable<Integer[]>{
+    LinkedList <Integer[]> polyList = new LinkedList<>();
+
+    public MyPolynomial(Integer[][] terms){
+        for (Integer[] term: terms){
+            polyList.add(term);
+        }
+    }
+
+    public MyPolynomial(MyPolynomial p){
+
+        for(Integer[] term: p){
+            polyList.add(term);
+        }
+    }
+
+    public Iterator<Integer[]> iterator(){
+        return polyList.iterator();
+    }
+
+    public int degree(){
+        return polyList.get(0)[1];
+    }
+
+    public String toString(){
+        String result = "";
+       
+
+        return result;
     }
 
 }
