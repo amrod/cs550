0968df49f67eb17c9b307f4e122d0f1e26849efa
Amaury Rodriguez <amaury.rodriguez@gmail.com>
2015-10-04 00:52:35 -0400

    Add tests for MyPolynomial

---
+++ hw2/test/edu/sju/MyPolynomialTest.java
@@ -0,0 +1,28 @@
+package edu.sju;
+
+import junit.framework.TestCase;
+
+
+public class MyPolynomialTest extends TestCase {
+    MyPolynomial poly1;
+    MyPolynomial poly2;
+
+    @Override
+    protected void setUp() throws Exception {
+        poly1 = new MyPolynomial(new Integer[][] {{10, 100}, {1, 14}, {1, 0}});
+        poly2 = new MyPolynomial(new Integer[][] {{4, 5}, {-2, 3}, {-100, 2}, {40, 1}});
+    }
+
+    public void testDegree() throws Exception {
+        assertEquals(poly1.degree(), 100);
+    }
+
+    public void testToString1() throws Exception {
+        assertEquals("10x^100 + x^14 + 1", poly1.toString());
+
+    }
+
+    public void testToString2() throws Exception {
+        assertEquals("4x^5 - 2x^3 - 100x^2 + 40x", poly2.toString());
+    }
+}
