4b1def751243a6ebce01cb16f0f3082cddd0cf3a
Amaury Rodriguez <amaury.rodriguez@gmail.com>
2015-11-12 13:16:17 -0500

    Document Interpreter

--- hw4/src/edu/sju/Interpreter.java
+++ hw4/src/edu/sju/Interpreter.java
@@ -5,19 +5,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/***
+ * Interpreter class facilitates parsing of an instruction file to create an
+ * AVL tree.
+ */
 public class Interpreter {
-    ArrayList<String> instructions;
+    private ArrayList<String> instructions = null;
 
+    /***
+     * Constructs instance from pre-read instructions array.
+     * @param i
+     */
     public void Interpreter(ArrayList<String> i){
         this.instructions = i;
     }
 
+    /***
+     * Reads an instruction file into internal variable.
+     * @param filePath Path to the instruction file to load.
+     * @throws IOException if there is an error reading the file.
+     */
     public void loadInstructionFile(String filePath) throws IOException{
-        this.instructions = readInstructons(filePath);
-    }
-
-    private ArrayList<String> readInstructons(String path) throws IOException {
-        FileReader fileReader = new FileReader(path);
+        FileReader fileReader = new FileReader(filePath);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         ArrayList<String> instructions = new ArrayList<String>();
 
@@ -29,9 +38,16 @@ public class Interpreter {
         }
 
         fileReader.close();
-        return instructions;
+        this.instructions = instructions;
     }
 
+    /***
+     * Test if the given instruction parts are valid.
+     * @param parts An array of either 1 or 2 elements. Element 0 must be a
+     *              supported instruction. Element 1 may be an argument. If
+     *              no argument is needed, only element 0 is required.
+     * @return true if the instruction appears valid, false if not.
+     */
     private boolean isValidInstruction(String[] parts){
 
         if (parts.length < 1 || parts.length > 2)
@@ -42,6 +58,12 @@ public class Interpreter {
         return true;
     }
 
+    /***
+     * Splits a string into command and argument (if an argument is present).
+     * @param line Line of text read from an instruction file.
+     * @return String array with the command parts identified.
+     * @throws Exception if the command appears to be invalid.
+     */
     private String[] makeCommandParts(String line) throws Exception{
         String[] parts = line.split("\t");
 
@@ -59,9 +81,18 @@ public class Interpreter {
         return sb.toString();
     }
 
+    /***
+     * Runs the interpreter over the
+     * @return The AVL tree created with the loaded instructions.
+     * @throws Exception If instruction file has not been loaded or if there
+     * is an error interpreting an instruction.
+     */
     public AVLTree<Integer> run() throws Exception{
-        AVLTree<Integer> tree = new AVLTree<Integer>();
+        if (instructions == null)
+            throw new Exception(
+                    "You must call loadInstructionFile() before running the interpreter.");
 
+        AVLTree<Integer> tree = new AVLTree<Integer>();
         String[] parts;
         for (String str : instructions) {
             parts = makeCommandParts(str);
@@ -76,6 +107,5 @@ public class Interpreter {
                 tree.runMethod(parts[0]);
         }
         return tree;
-
     }
 }
