3b7b070cfe5ba31cc006d5b288cda3f621259d67
Amaury Rodriguez <amaury.rodriguez@gmail.com>
2015-12-02 23:11:01 -0500

    Add constructor

--- hw6/src/sju/edu/DepthFirstSearch.java
+++ hw6/src/sju/edu/DepthFirstSearch.java
@@ -23,5 +23,30 @@ public class DepthFirstSearch {
     private int discoverIndex = 0;
     /** The index that indicates the finish order. */
     private int finishIndex = 0;
+
+    /**
+     * Construct the depth-first search of a Graph
+     * starting at vertex 0 and visiting the start vertices in
+     * ascending order.
+     * @param graph The graph
+     */
+    public DepthFirstSearch(Graph graph) {
+        this.graph = graph;
+        int n = graph.getNumV();
+        parent = new int[n];
+        visited = new boolean[n];
+        discoveryOrder = new int[n];
+        finishOrder = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                depthFirstSearch(i);
+            }
+        }
     }
+
+
+}
 
